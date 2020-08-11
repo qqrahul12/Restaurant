eddf232e5d13e0ef0f08ee423c201d5a59a6fb8d
qqrahul12 <68814060+qqrahul12@users.noreply.github.com>
2020-08-12 04:12:36 +0530

    Update normal_bill_confirm.php

--- project/normal_bill_confirm.php
+++ project/normal_bill_confirm.php
@@ -13,38 +13,13 @@
     $amount = $_SESSION["total"];
     
     mysqli_query($con,"INSERT into bill(type,amount) values('$type',$amount)");
-    $info = mysqli_query($con,"SELECT id from bill ORDER BY id DESC LIMIT 1");
+    $info = mysqli_query($con,"SELECT id,day from bill ORDER BY id DESC LIMIT 1");
     $row = mysqli_fetch_row($info);
     $bill_id = $row[0];
-?>
 
-<head>
-    <link rel="stylesheet" href="css/card.css">
-    <link rel="stylesheet" href="css/button.css">
-    <link rel="stylesheet" href="css/input.css">
-    <script type="text/javascript" src="js/redirect.js" ></script>
-</head>
-<body>
-<div class="card">
-    <h2>Your Bill</h2>
-    <table align="center">
-        <tr>
-            <td>Bill id</td>
-            <td><?php echo $bill_id; ?></td>
-        </tr>
-        <tr>
-            <td>Bill Date</td>
-            <td><?php echo(date("Y-m-d")); ?></td>
-        </tr>
-        <tr>
-            <td>Bill type</td>
-            <td><?php echo $type; ?></td>
-        </tr>
-        <tr>
-            <td>Bill amount</td>
-            <td><?php echo $amount; ?></td>
-        </tr>
-    </table><hr>
-    <button type="submit" onclick="redirect('admin_home.php')">Back to Home</button>
-</div>
-</body>
+    $_SESSION["bill_id"] = $bill_id;
+    $_SESSION["type"]  = $type;
+    $_SESSION["date"]  = $row[1];
+
+    header("Location:normal_bill_detail.php");
+?>
