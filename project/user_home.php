779a7f0e2573c49eaa98418208d5feabc60b3dc3
qqrahul12 <68814060+qqrahul12@users.noreply.github.com>
2020-08-12 03:55:29 +0530

    updated user home page
    added card class to give symmetric look

--- project/user_home.php
+++ project/user_home.php
@@ -13,6 +13,7 @@
 
   <link rel= "stylesheet" type="text/css"  href="css/rc.css">
   <link rel= "stylesheet" type="text/css"  href="css/button.css">
+  <link rel= "stylesheet" type="text/css"  href="css/card.css">
   <script src = "js/redirect.js"> </script>
 
   <style type="text/css">
@@ -25,29 +26,21 @@
 
 </head>
 
-
-<body>
-	<?php include 'welcome.php'?>
-	<div class="center">
-	<div id="menu">
-	<div class="vertispace">
+<body style="background-color: #888888">
+	<div class="card">
+    <h2 style="text-align: left"><?php echo "Hello " . $_SESSION['name']; ?></h2>
 	<button class="btn btn-primary" onclick="redirect('menu.php')" type="submit">Check menu</button>
-</div>
-</div>
+  <br><br>
 
-	<div class="vertispace">
 	<button class="button" onclick="redirect('book_table.php')" type="submit">Book table</button>
-</div>
-	<div class="vertispace">
+  <br><br>
+
 	<button class="button" onclick="redirect('order.php')" type="submit">Order Online</button>
-</div>
+  <br><br>
 
-	<div class="vertispace">
 	<button class="btn btn-primary" onclick="redirect('history.php')" type="submit">Your orders</button>
-</div>
+  <br><br>
 
-	<div class="vertispace">
 	<button class="btn btn-primary" onclick="redirect('logout.php')" type="submit">logout</button>
-</div>
 </div>
 </body>
