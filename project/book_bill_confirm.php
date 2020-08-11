38c49d7b78f53c9c9faf4f9aa78be0d8924d5a50
qqrahul12 <68814060+qqrahul12@users.noreply.github.com>
2020-08-12 04:10:27 +0530

    Update book_bill_confirm.php

--- project/book_bill_confirm.php
+++ project/book_bill_confirm.php
@@ -12,57 +12,17 @@
     $type  = $_SESSION["type"];
     $user = $_SESSION["user"];
     $amount = $_SESSION["total"];
+    $count  = $_SESSION["count"];
 
     mysqli_query($con,"INSERT into bill(user_id,type,type_id,amount) values($user,'$type',$book_id,$amount)");
     mysqli_query($con,"UPDATE book_history set pending = 0 where id = $book_id");
-    $info2 = mysqli_query($con,"SELECT id from bill ORDER BY id DESC LIMIT 1");
+    $info = mysqli_query($con,"SELECT avl_tables from vals");
+    $upd = mysqli_fetch_row($info)[0]+$count;
+    mysqli_query($con,"UPDATE vals set avl_tables = $upd");
+    $info2 = mysqli_query($con,"SELECT id,day from bill ORDER BY id DESC LIMIT 1");
     $row = mysqli_fetch_row($info2);
-    $bill_id = $row[0];
-?>
+    $_SESSION['bill_id'] = $row[0];
+    $_SESSION["date"] = $row[1];
 
-<head>
-    <link rel="stylesheet" href="css/card.css">
-    <link rel="stylesheet" href="css/button.css">
-    <link rel="stylesheet" href="css/input.css">
-    <script type="text/javascript" src="js/redirect.js" ></script>
-</head>
-<body>
-<div class="card">
-    <h2>Your Bill</h2>
-    <table align="center">
-        <tr>
-            <td>Bill id</td>
-            <td><?php echo $bill_id; ?></td>
-        </tr>
-        <tr>
-            <td>Bill Date</td>
-            <td><?php echo(date("Y-m-d")); ?></td>
-        </tr>
-        <tr>
-            <td>user id</td>
-            <td><?php echo $user; ?></td>
-        </tr>
-        <tr>
-            <td>Bill type</td>
-            <td><?php echo $type; ?></td>
-        </tr>
-        <tr>
-            <td>Booking id</td>
-            <td><?php echo $book_id; ?></td>
-        </tr>
-        <tr>
-            <td>Bill amount</td>
-            <td><?php echo $amount; ?></td>
-        </tr>
-    </table><hr>
-    <button type="submit" onclick="redirect('admin_home.php')">Back to Home</button>
-</div>
-</body>
-
-<?php
-    unset($_SESSION["book_id"]);
-    unset($_SESSION["type"]);
-    unset($_SESSION["user"]);
-    unset($_SESSION["total"]);
-
+    header("Location:book_bill_detail.php");
 ?>
