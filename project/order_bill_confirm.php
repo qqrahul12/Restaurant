e243393ff36d8673d6f01ba59f91e86d2b76959e
qqrahul12 <68814060+qqrahul12@users.noreply.github.com>
2020-08-12 04:08:28 +0530

    date added

--- project/order_bill_confirm.php
+++ project/order_bill_confirm.php
@@ -25,47 +25,16 @@
 
     mysqli_query($con,"INSERT into bill(user_id,type,type_id,amount) values($user,'$type',$id,$amount)");
     mysqli_query($con,"UPDATE order_history set pending = 0 where id = $id");
-    $info2 = mysqli_query($con,"SELECT id from bill ORDER BY id DESC LIMIT 1");
+    $info2 = mysqli_query($con,"SELECT * from bill ORDER BY id DESC LIMIT 1");
     $row = mysqli_fetch_row($info2);
     $bill_id = $row[0];
+    $_SESSION["date"] = $row[1];
+    $_SESSION["user"] = $user;
+    $_SESSION["type"] = $type;
+    $_SESSION["order_id"]= $id;
+    $_SESSION["amount"] = $amount;
+    $_SESSION["bill_id"] = $bill_id;
+
+    header("Location:order_bill_detail.php");
 }
 ?>
-
-<head>
-    <link rel="stylesheet" href="css/card.css">
-    <link rel="stylesheet" href="css/button.css">
-    <link rel="stylesheet" href="css/input.css">
-    <script type="text/javascript" src="js/redirect.js" ></script>
-</head>
-<body>
-<div class="card">
-    <h2>Your Bill</h2>
-    <table align="center">
-        <tr>
-            <td>Bill id</td>
-            <td><?php echo $bill_id; ?></td>
-        </tr>
-        <tr>
-            <td>Bill Date</td>
-            <td><?php echo(date("Y-m-d")); ?></td>
-        </tr>
-        <tr>
-            <td>user id</td>
-            <td><?php echo $user; ?></td>
-        </tr>
-        <tr>
-            <td>Bill type</td>
-            <td><?php echo $type; ?></td>
-        </tr>
-        <tr>
-            <td>Order id</td>
-            <td><?php echo $id; ?></td>
-        </tr>
-        <tr>
-            <td>Bill amount</td>
-            <td><?php echo $amount; ?></td>
-        </tr>
-    </table><hr>
-    <button type="submit" onclick="redirect('admin_home.php')">Back to Home</button>
-</div>
-</body>
