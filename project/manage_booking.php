16820dd71aa37e1e6f45a330f21fc11c7bd4671c
qqrahul12 <68814060+qqrahul12@users.noreply.github.com>
2020-08-12 05:42:05 +0530

    updated with table stat and booking charge

--- project/manage_booking.php
+++ project/manage_booking.php
@@ -30,9 +30,10 @@
 	<link rel="stylesheet" href="css/card.css">
 	<link rel="stylesheet" href="css/button.css">
 	<link rel="stylesheet" href="css/rc.css">
+	<link rel="stylesheet" href="css/input.css">
 </head>
 <body>
-<div class="card">
+<div class="card" id="cur_book">
 <p class="dish-name" id="1">Current Bookings</p>
 <?php
 	$info1 = mysqli_query($con,"SELECT * from book_history where pending=1 order by id DESC ");
@@ -71,11 +72,16 @@
 			echo '<br>';
 		}
 	}
-?>
+?><hr>
+<a href="#book_charge">Booking Charges</a>
+<a href="#prev_book">Previous Bookings</a>
+<a href="#book_stat">Booking Stats</a>
+<a href="#table_stat">Table stats</a>
+<a href="admin_home.php">Back</a>
 </div>
 <br>
 
-<div class="card">
+<div class="card" id="prev_book">
 <p class="dish-name" id="1">Completed Bookings</p>
 <?php
 	$info1 = mysqli_query($con,"SELECT * from book_history where pending=0 order by id DESC ");
@@ -112,11 +118,16 @@
 			echo '<br>';
 		}
 	}
-?>
+?><hr>
+<a href="#book_charge">Booking Charges</a>
+<a href="#cur_book">Current Bookings</a>
+<a href="#book_stat">Booking Stats</a>
+<a href="#table_stat">Table stats</a>
+<a href="admin_home.php">Back</a>
 </div>
 <br>
 
-<div class="card">
+<div class="card" id = "book_stat">
 <p class="dish-name" id="1">Booking Stastics</p>
 <?php
 
@@ -152,7 +163,83 @@
 		<td><?php echo $daily_charge;?></td>';
 	</tr>';
 
-</table>
-<br>
+</table><hr>
+<a href="#book_charge">Booking Charges</a>
+<a href="#prev_book">Previous Bookings</a>
+<a href="#cur_book">Current Booking</a>
+<a href="#table_stat">Table stats</a>
 <a href="admin_home.php">Back</a>
+</div>
+<?php
+    $info = mysqli_query($con,"SELECT charge from booking_charge ");
+    $charge=array();$j=0;
+    while($row=$info->fetch_assoc())
+    			$charge[$j++]=$row["charge"];
+    $info = mysqli_query($con,"SELECT * from vals ");
+    $row = mysqli_fetch_row($info);
+    $tt = $row[0];
+    $at= $row[1];
+    $mt = $row[2];
+
+?>
+<div class="card" id = "book_charge">
+	<h2>Booking Charges</h2>
+	<table align="center" style="margin-top: 20px">
+	<tr>
+		<th>Tables</th>
+		<th>Charge</th>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td><?php echo $charge[0] ?></td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td><?php echo $charge[1] ?></td>
+	</tr>
+	<tr>
+		<td>3</td>
+		<td><?php echo $charge[2] ?></td>
+	</tr>
+</table><hr>
+<form action="edit_booking_charge.php" method="POST">
+	<label for="count">count</label><br>
+	<input type="number" name="count" class="ipt" id="count" min ="1" max = "<?php echo $mt?>" step = "1" required><br>
+	<label for="charge">charge</label><br>
+	<input type="number" name="charge" class="ipt" id = "charge" min ="50" step = "50" required><br><br>
+	<button type="submit">change now</button>
+</form> <hr>
+<a href="#cur_book">Current Bookings</a>
+<a href="#prev_book">Previous Bookings</a>
+<a href="#book_stat">Booking Stats</a>
+<a href="#table_stat">Table stats</a>
+<a href="admin_home.php">Back</a>
+</div>
+<div class="card" id="table_stat">
+	<h2>Table stats</h2>
+	<table align="center" style="margin-top: 20px">
+	<tr>
+		<td>Total tables</td>
+		<td><?php echo $tt ?></td>
+	</tr>
+	<tr>
+		<td>Available tables</td>
+		<td><?php echo $at ?></td>
+	</tr>
+	<tr>
+		<td>Max tables can be booked</td>
+		<td><?php echo $mt ?></td>
+	</tr>
+</table> <hr>
+<form action="edit_total_table.php" method="POST">
+	<label for="tt">new Total tables</label><br>
+	<input type="number" name="tt" class="ipt" id="tt" min ="10" step = "5" required> 
+	<br><br>
+	<button type="submit">change now</button>
+</form><hr>
+<a href="#book_charge">Booking Charges</a>
+<a href="#prev_book">Previous Bookings</a>
+<a href="#book_stat">Booking Stats</a>
+<a href="#cur_book">Current Booking</a>
+<a href="admin_home.php">Back</a> 
 </div>
